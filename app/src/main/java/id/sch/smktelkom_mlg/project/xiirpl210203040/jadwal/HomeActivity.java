5b8ca5d80290da3f7cbe636c45db2609d876d9de
JihanFieD <fiejihan03@gmail.com>
2016-11-18 17:00:47 +0700

    Fragment

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl210203040/jadwal/HomeActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl210203040.jadwal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl210203040/jadwal/activity/ReminderFragment.java
@@ -0,0 +1,47 @@
+package id.sch.smktelkom_mlg.project.xiirpl210203040.jadwal.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.project.xiirpl210203040.jadwal.R;
+
+/**
+ * Created by SMK Telkom SP Malang on 18/11/2016.
+ */
+
+public class ReminderFragment extends Fragment
+{
+    public ReminderFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_reminder, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
